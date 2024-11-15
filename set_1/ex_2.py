6d1ffc285bb4bfb7788d85b4e3ae6303be2304cf
Craig Giddings <randomhero_345@hotmail.com>
2024-11-15 16:56:01 -0500

    Updated exercise 2

--- set_1/ex_2.py
+++ set_1/ex_2.py
@@ -1,17 +1,18 @@
-from binascii import unhexlify, hexlify
+from binascii import unhexlify, hexlify, b2a_hex, a2b_hex
 
 def bxor(a, b):
     """bitwise XOR of bytestrings"""
-    return hex(int(a, 16) ^ int(b, 16))
-    # return bytes([x^y for (x, y) in zip(a,b)])
+    return bytes([x^y for (x, y) in zip(a,b)])
 
-hex_str = "1c0111001f010100061a024b53535009181c"
-data = "686974207468652062756c6c277320657965"
-expected_result = "746865206b696420646f6e277420706c6179"
 
-result = bxor(hex_str, data)
-print(result[2:])
-print(type(result))
+if __name__ == "__main__":
+    hex_str = unhexlify(b"1c0111001f010100061a024b53535009181c")
+    data = unhexlify(b"686974207468652062756c6c277320657965")
+    expected_result = b"746865206b696420646f6e277420706c6179"
 
-# Remove 0x from result output
-assert result[2:] == expected_result, "Hex strings do not match"
+    result = bxor(hex_str, data)
+    # print(result)
+    # print("\n")
+    # print(hexlify(result))
+    # Remove 0x from result output
+    assert hexlify(result) == expected_result, "Hex strings do not match"
--- set_1/sandbox.py
+++ set_1/sandbox.py
@@ -1,30 +1,60 @@
-# Script to fuck around on
-from binascii import a2b_base64
+# # Script to fuck around on
+# from binascii import a2b_base64
+# from ex_6 import hamming_distance
+
+# x = b"HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS"
+# y = b"BgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYG"
+
+# # Decode text from base64
+# x = a2b_base64(x)
+# y = a2b_base64(y)
+
+# # Calculate hamming distance
+# keysize = 3
+
+# # Step 1: Divide the cipher text into blocks of length equal to the key size guessed above
+# chunk_x = [x[i:i + keysize] for i in range(0, len(x), keysize)]
+# chunk_y = [y[j:j + keysize] for j in range(0, len(y), keysize)]
+
+# # Step 2: Calculate the Hamming distance by dividing it by the key size
+# distances = []
+# for i, j in zip(chunk_x, chunk_y):
+#     distances.append(hamming_distance(i, j))
+
+# # Step 3: Normalize the distance by dividing it by the key size (Repeat for 4 to 5 blocks)
+# normalized_distances = list(map(lambda x: x / keysize, distances))
+
+# # Step 4: Take the average of the calculated distances
+# average_distance = sum(normalized_distances) / keysize
+# print(average_distance) 
+
+
+from binascii import a2b_base64, hexlify
 from ex_6 import hamming_distance
 
 x = b"HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS"
-y = b"BgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYG"
+# y = b"BgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYG"
 
 # Decode text from base64
 x = a2b_base64(x)
-y = a2b_base64(y)
+# y = a2b_base64(y)
 
 # Calculate hamming distance
-keysize = 3
+keysize = 4
 
 # Step 1: Divide the cipher text into blocks of length equal to the key size guessed above
 chunk_x = [x[i:i + keysize] for i in range(0, len(x), keysize)]
-chunk_y = [y[j:j + keysize] for j in range(0, len(y), keysize)]
+# chunk_y = [y[j:j + keysize] for j in range(0, len(y), keysize)]
 
 # Step 2: Calculate the Hamming distance by dividing it by the key size
 distances = []
-for i, j in zip(chunk_x, chunk_y):
-    distances.append(hamming_distance(i, j))
+for chunk in range(0, len(chunk_x) - 1):
+    print(chunk_x[chunk], chunk_x[chunk + 1])
+    
 
-# Step 3: Normalize the distance by dividing it by the key size (Repeat for 4 to 5 blocks)
-normalized_distances = list(map(lambda x: x / keysize, distances))
+# # Step 3: Normalize the distance by dividing it by the key size (Repeat for 4 to 5 blocks)
+# normalized_distances = list(map(lambda x: x / keysize, distances))
 
-# Step 4: Take the average of the calculated distances
-average_distance = sum(normalized_distances) / keysize
-print(average_distance) 
-
+# # Step 4: Take the average of the calculated distances
+# average_distance = sum(normalized_distances) / keysize
+# print(average_distance) 
